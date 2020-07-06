69b93b7ad036475bc0746c389102450d28b8dc1f
Bruno Sabia <brunosabia1@gmail.com>
2020-07-06 17:59:29 -0300

    Adding class 27

---
+++ 27 Enums/Enums/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Enums.Entities.Enums;
+
+namespace Enums.Entities 
+{
+    class Order
+    {
+        public int Id;
+        public DateTime Moment;
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return "ID: " + Id + ", Moment: " + Moment + ", Status: " + Status + ".";
+        }
+
+
+
+    }
+}
---
+++ 27 Enums/Enums/Program.cs
@@ -0,0 +1,47 @@
+﻿using Enums.Entities;
+using Enums.Entities.Enums;
+using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+            Console.WriteLine(order.Status);
+
+            //converter ENUM para String:
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+
+
+            //Converter String para ENUM:
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+
+            //testes
+            Console.WriteLine("Pagamento recebido:");
+            string received = Console.ReadLine();
+            
+            if(received != "nao" && received != "não"  )
+            {
+                order.Status = Enum.Parse<OrderStatus>("Processing");
+                Console.WriteLine(order);
+            }
+            
+            order.Status = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(order);
+        
+        }
+    }
+}
