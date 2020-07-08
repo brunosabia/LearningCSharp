51a23375c29aa0b04ce38fd1f8079724246f4d56
Bruno Sabia <brunosabia1@gmail.com>
2020-07-08 16:57:53 -0300

    Adding Class 29

---
+++ 29 StringBuilder/StringBuilder/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EXStringBuilder
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+
+}
