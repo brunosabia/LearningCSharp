d2cfa315b0473c321260b587a3f5622ed7196db6
Bruno Sabia <brunosabia1@gmail.com>
2020-07-10 17:45:53 -0300

    Adding Inheritance Class 31

---
+++ 31 Herança/Herança/Entities/BusinessAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+        // construtor aproveitando o construtor da superclasse:
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+                Balance += amount;
+        }
+    }
+}
---
+++ 31 Herança/Herança/Program.cs
@@ -0,0 +1,16 @@
+﻿using Herança.Entities;
+using System;
+
+namespace Herança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            //Balance é uma variável protected. Por isso a linha abaixo não funciona.
+            //account.Balance = 200.0;
+        }
+    }
+}
