a07690cd36a1d857aa3be6a4cabd2bd098702e6c
Bruno Sabia <brunosabia1@gmail.com>
2020-07-24 17:18:38 -0300

    Adding Class 38

---
+++ 38 Exceptions/38 Exceptions/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace _38_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not allowed.");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error! " + e.Message);
+            }
+            finally
+            {
+                //fechar arquivos e conexões com banco
+            }
+
+
+
+            //exemplo melhor do uso do finally:
+
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\Users\bruno\OneDrive\Área de Trabalho\ideias de nomes.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if(fs != null)
+                {
+                    fs.Close();
+                }
+            }
+
+        }
+    }
+}
