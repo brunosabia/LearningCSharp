bb1e097d0f05639ec8763a2055ea958e5247f42a
Bruno Sabia <brunosabia1@gmail.com>
2020-08-12 16:38:47 -0300

    Adding Class 43

---
+++ 43 usingBlock/43 usingBlock/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _43_usingBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+
+
+            try
+            {
+                //o bloco using fecha automaticamente as instancias quando o bloco dele é terminado.
+                using (FileStream fs = new FileStream(path, FileMode.Append))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }catch(IOException e)
+            {
+                Console.WriteLine("An error occurred:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
