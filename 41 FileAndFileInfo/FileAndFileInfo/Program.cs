dd3efdb12d087f0c97d8a7b35ac9ff94cb5c1811
Bruno Sabia <brunosabia1@gmail.com>
2020-08-12 16:09:20 -0300

    Adding Class 41

---
+++ 41 FileAndFileInfo/FileAndFileInfo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace FileAndFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file2.txt";
+
+            try
+            {
+
+                //FileInfo
+                //instancia um FileInfo informando o caminho do arquivo
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                //Copia o conteudo dessa instancia para o caminho destino
+                fileInfo.CopyTo(targetPath);
+                
+
+                //File
+
+                //sem criar nenhuma instancia, ele apenas lê o conteudo do arquivo
+                string[] lines = File.ReadAllLines(sourcePath);
+                //e imprime
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
