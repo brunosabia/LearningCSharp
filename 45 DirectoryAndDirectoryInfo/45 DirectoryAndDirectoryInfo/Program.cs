f361321a97c01b8d1d332d7cc8c7f4b2b1328e22
Bruno Sabia <brunosabia1@gmail.com>
2020-08-13 15:58:10 -0300

    Adding Class 45

---
+++ 45 DirectoryAndDirectoryInfo/45 DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _45_DirectoryAndDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\myfolder";
+
+            try
+            {   
+                //LISTAR AS PASTAS
+                //ao usar o Directory.EnumerateDirectories ele retorna uma lista do tipo IEnumerable, entao 
+                //é criado um IEnumerable para receber a lista de folders e tambem pode ser usado o "var", por exemplo:
+                // var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                //*.* significa com qualquer nome e com qualquer extensão
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //LISTAR OS ARQUIVOS
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //CRIAR UMA PASTA
+                Directory.CreateDirectory(path + "\\newfolder");
+
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
