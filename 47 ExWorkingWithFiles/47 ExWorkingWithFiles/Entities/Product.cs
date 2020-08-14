9e20cef5c85962e8a83f32d8368b61a45a9a4384
Bruno Sabia <brunosabia1@gmail.com>
2020-08-14 17:15:17 -0300

    Adding Class 47

---
+++ 47 ExWorkingWithFiles/47 ExWorkingWithFiles/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace _47_ExWorkingWithFiles.Entities
+{
+    class Product
+    {
+        public string name { get; private set; }
+        public double price { get; private set; }
+        public int quantity { get; private set; }
+
+
+
+        public Product() { }
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public double totalPrice()
+        {
+            return price * quantity;
+        }
+    }
+}
---
+++ 47 ExWorkingWithFiles/47 ExWorkingWithFiles/Program.cs
@@ -0,0 +1,57 @@
+﻿using _47_ExWorkingWithFiles.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace _47_ExWorkingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\temp\myfolder\newfolder\file.csv";
+            string targetPath = @"C:\temp\myfolder\newfolder\out\summary.csv";
+
+            try
+            {
+                List<Product> prods = new List<Product>();
+
+                using(FileStream fs = new FileStream(sourcePath, FileMode.Open))
+                {
+                    using(StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string[] items = null;
+                            items = sr.ReadLine().Split(',');
+                            string name = items[0];
+                            double price = double.Parse(items[1],CultureInfo.InvariantCulture);
+                            int quantity = int.Parse(items[2]);
+
+                            Product product = new Product(name, price, quantity);
+                            prods.Add(product);
+                            
+                        }
+                    }
+                }
+
+                Directory.CreateDirectory(@"C:\temp\myfolder\newfolder\out");
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (Product product in prods)
+                    {
+                        sw.WriteLine(product.name + ", " + product.totalPrice().ToString("F2",CultureInfo.InvariantCulture));
+                    }
+                }
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
