24d2e8737d2aa7fd162aaff38d3661aca18bd987
Bruno Sabia <brunosabia1@gmail.com>
2020-08-17 19:00:43 -0300

    Adding Class 48

---
+++ 48 Interface/48 Interface/Program.cs
@@ -0,0 +1,24 @@
+﻿using _48_Interface.Entities;
+using System;
+using System.Globalization;
+
+namespace _48_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter rental data");
+            Console.WriteLine("Car model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm: ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(),"dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm: ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+        }
+    }
+}
---
+++ 48 Interface/48 Interface/Services/RentalService.cs
@@ -0,0 +1,25 @@
+﻿using _48_Interface.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _48_Interface.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+    
+    
+        public void ProcessInvoice(CarRental carRental)
+        {
+
+        }
+    }
+}
