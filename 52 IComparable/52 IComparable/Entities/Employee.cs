2c9d08377b169605219f677865ac6463e77af224
Bruno Sabia <brunosabia1@gmail.com>
2020-08-25 15:57:46 -0300

    Only sorting the names on the file

---
+++ 52 IComparable/52 IComparable/Entities/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace _52_IComparable.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vector = csvEmployee.Split(',');
+            Name = vector[0];
+            Salary = double.Parse(vector[1], CultureInfo.InvariantCulture);
+        }
+
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 52 IComparable/52 IComparable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _52_IComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file3.txt";
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+
+                    list.Sort();
+
+                    foreach (string str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
