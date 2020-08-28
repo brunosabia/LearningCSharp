56d124891e6c6ca5d9b509c624c81c4fb933b48b
Bruno Sabia <brunosabia1@gmail.com>
2020-08-28 16:26:02 -0300

    Adding Class 55

---
+++ 55 GetHashCodeAndEquals/55 GetHashCodeAndEquals/Program.cs
@@ -0,0 +1,22 @@
+﻿using _55_GetHashCodeAndEquals.Entities;
+using System;
+
+namespace _55_GetHashCodeAndEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client("Maria", "maria@gmail.com");
+
+            Client b = new Client("Alex", "alex@gmail.com");
+            Client c = new Client("Manuela", "maria@gmail.com");
+
+            Console.WriteLine(a.Equals(c));
+            Console.WriteLine(a == c);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+            Console.WriteLine(c.GetHashCode());
+        }
+    }
+}
