8d473488e518b55c6905fe0f84b38ac50a9f1dc5
Bruno Sabia <brunosabia1@gmail.com>
2020-08-28 16:56:31 -0300

    Simple use of a Sorted Set

--- 56 HashSetAndSortedSet/56 HashSetAndSortedSet/Program.cs
+++ 56 HashSetAndSortedSet/56 HashSetAndSortedSet/Program.cs
@@ -7,18 +7,38 @@ namespace _56_HashSetAndSortedSet
     {
         static void Main(string[] args)
         {
-            HashSet<string> set = new HashSet<string>();
+            SortedSet<int> a = new SortedSet<int> { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int> { 5, 6, 7, 8, 9, 10 };
 
-            set.Add("TV");
-            set.Add("Notebook");
-            set.Add("Tablet");
+            PrintCollection(a);
 
-            Console.WriteLine(set.Contains("Notebook"));
+            //Criando a coleção C e adicionando os valores de A ao C
+            SortedSet<int> c = new SortedSet<int>(a);
+            //Union : vou inserir no C todos os elementos do B que já nao estejam em C:
+            c.UnionWith(b);
+            PrintCollection(c);
 
-            foreach (string item in set)
+
+            //Intersection - só adiciona os elementos que se repetem nas duas coleções
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+
+            //difference - só os elementos que a coleção A tem e a colecao B não tem
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
             {
-                Console.WriteLine(item);
+                Console.Write(obj + " ");
             }
+            Console.WriteLine();
         }
     }
 }
