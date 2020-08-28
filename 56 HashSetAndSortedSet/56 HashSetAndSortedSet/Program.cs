c2d74140c25d7033fd3d832e008f822a07a0e5c5
Bruno Sabia <brunosabia1@gmail.com>
2020-08-28 16:42:12 -0300

    Simple use of a HashSet

---
+++ 56 HashSetAndSortedSet/56 HashSetAndSortedSet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _56_HashSetAndSortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
