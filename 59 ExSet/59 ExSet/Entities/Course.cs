be4f36305aea8c84eac0b4552997e15e334ff777
Bruno Sabia <brunosabia1@gmail.com>
2020-09-02 15:57:46 -0300

    Adding my solution to Class 59

---
+++ 59 ExSet/59 ExSet/Entities/Course.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace _59_ExSet.Entities
+{
+    class Course
+    {
+        
+        HashSet<User> Crs = new HashSet<User>();
+
+        public void addUser(int code)
+        {
+            Crs.Add(new User(code));
+        }
+
+        public void printCourse()
+        {
+            foreach (User item in Crs)
+            {
+                Console.WriteLine("UserCode: " + item.UserCode);
+            }
+        }
+        
+        public int Count()
+        {
+            return Crs.Count;
+        }
+        
+    }
+}
---
+++ 59 ExSet/59 ExSet/Program.cs
@@ -0,0 +1,48 @@
+﻿using _59_ExSet.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace _59_ExSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course A = new Course();
+            Course B = new Course();
+            Course C = new Course();
+            Course Total = new Course();
+
+            Console.Write("How many students for course A?");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int code = int.Parse(Console.ReadLine());
+                A.addUser(code);
+                Total.addUser(code);
+            }
+
+            Console.Write("How many students for course B?");
+            int m = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                int code = int.Parse(Console.ReadLine());
+                B.addUser(code);
+                Total.addUser(code);
+            }
+
+            Console.Write("How many students for course C?");
+            int k = int.Parse(Console.ReadLine());
+            for (int i = 0; i < k; i++)
+            {
+                int code = int.Parse(Console.ReadLine());
+                C.addUser(code);
+                Total.addUser(code);
+            }
+
+            Console.WriteLine("Total students: " + Total.Count());
+
+       
+        }
+    }
+}
