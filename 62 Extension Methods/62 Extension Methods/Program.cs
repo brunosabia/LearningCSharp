73e2f424aa81ee19190dc6f4fbcb23a8e8b1c9c0
Bruno Sabia <brunosabia1@gmail.com>
2020-09-08 16:45:23 -0300

    Adding Class 62

---
+++ 62 Extension Methods/62 Extension Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _62_Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 08, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
