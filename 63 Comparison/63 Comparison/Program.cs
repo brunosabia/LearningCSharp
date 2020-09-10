9df0028cecaf6a6dc9ce2507e5ad6dbb8a08587c
Bruno Sabia <brunosabia1@gmail.com>
2020-09-10 15:09:07 -0300

    Final version with the Inline Lambda expression used for the Comparison

--- 63 Comparison/63 Comparison/Program.cs
+++ 63 Comparison/63 Comparison/Program.cs
@@ -14,8 +14,13 @@ namespace _63_Comparison
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            //Esta linha abaixo é chamada de COMPARISON - DELEGATE, onde o método SORT recebe um método(SEM OS PARENTESES) que passa as regras usadas, neste caso, para ordenar a lista.
-            list.Sort(CompareProducts);
+            //this arrow function below is the shortened version of the CompareProducts method that will be delegated to the Sort method as a rule to execute the Sorting of the list.
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+            //this comp expression above can also be put directly as a paremeter on the following Sort method like this:
+            //list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()))
+
+            list.Sort(comp);
 
 
 
@@ -26,11 +31,6 @@ namespace _63_Comparison
 
         }
 
-        //método responsavel pela regra da comparação entre os dois products pq tem uma implementação padrão COMPARISON
-        static int CompareProducts(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-        }
 
     }
 }
