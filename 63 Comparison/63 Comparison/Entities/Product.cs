78b49d4cc3442cf31d19bfbc9b531b0c058258d8
Bruno Sabia <brunosabia1@gmail.com>
2020-09-10 14:41:29 -0300

    The class Product would still need future updates to change the sorting rules

---
+++ 63 Comparison/63 Comparison/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Text;
+
+namespace _63_Comparison.Entities
+{
+    class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Product other)
+        {
+            return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        }
+    }
+}
---
+++ 63 Comparison/63 Comparison/Program.cs
@@ -0,0 +1,25 @@
+﻿using _63_Comparison.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace _63_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+            list.Sort();
+        
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
