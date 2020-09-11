9821412e9dcc339db5a480f592677acb6db41fe6
Bruno Sabia <brunosabia1@gmail.com>
2020-09-11 15:19:44 -0300

    Adding Class 64

---
+++ 64 FunctionalProgrammingArrowFunctions/64 FunctionalProgrammingArrowFunctions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _64_FunctionalProgrammingArrowFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+            C# é uma linguagem multiparadigma - pois é OO e Funcional tambem
+
+            Expressão Lambda = Função anônima de primeira classe = Arrow Function (fazem parte do tipo de programaçao Funcional)
+
+
+
+             
+             
+             */
+        }
+    }
+}
