36aecd6246531586155f5e52bca1d80d1f2cd8f3
Bruno Sabia <brunosabia1@gmail.com>
2020-09-14 12:32:50 -0300

    Adding Class 65

---
+++ 65 Delegate/Delegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using Delegate.Services;
+using System;
+
+namespace Delegate
+{
+    //essa é a declaracao pro delegate:
+    delegate double BinaryNumericOperation(double n1, double n2);
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            BinaryNumericOperation op1 = CalculationService.Max;
+
+            double result = op(a, b);
+            //double result = CalculationService.Sum(a, b);
+            Console.WriteLine(result);
+
+            double result1 = op1(a, b);
+            Console.WriteLine(result1);
+        }
+    }
+}
