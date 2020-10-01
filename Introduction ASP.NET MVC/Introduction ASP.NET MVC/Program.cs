ee1f52195925b27d6c0504e58a2010c5dfbb97ed
Bruno Sabia <brunosabia1@gmail.com>
2020-10-01 15:16:08 -0300

    Adding Introduction to ASP.NET Core MVC

---
+++ Introduction ASP.NET MVC/Introduction ASP.NET MVC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Introduction_ASP.NET_MVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //é um framework (Conjunto de ferramentas que te ajuda a criar aplicações de forma mais simples) para criação de aplicações WEB
+            /*
+                Criado pela Microsoft e a comunidade
+                Open Source
+                Roda no .Net Framework e .Net Core
+                Trabalha com uma estrutura bem definida(MVC)
+                -Controllers
+                -Views
+                -Models
+            */
+        }
+    }
+}
