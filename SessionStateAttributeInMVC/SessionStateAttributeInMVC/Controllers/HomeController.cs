966fdaa0cbef2a7ed38bc17c8a3f8b8e2dc0171f
Bruno <brunosabia1@gmail.com>
2021-01-08 13:04:16 -0300

    adding complementary project

---
+++ SessionStateAttributeInMVC/SessionStateAttributeInMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using SessionStateAttributeInMVC.Models;
+using System.Web.SessionState;
+
+namespace SessionStateAttributeInMVC.Controllers
+{
+    [ SessionState( SessionStateBehavior.Disabled)]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(EmpModel obj)
+        {
+            //store the session from user input and display into the view if session is enabled.
+            Session["Name"] = Convert.ToString(obj.Name);
+            return View();
+
+        }
+    }
+}
